d943643f22885347892593d14a9c27b5387c9112
nasjones <46979749+nasjones@users.noreply.github.com>
2021-09-23 15:58:42 -0400

    initial commit

---
+++ data.sql
@@ -0,0 +1,13 @@
+\c books_test
+CREATE TABLE books (
+  isbn text PRIMARY KEY,
+  amazon_url text,
+  author text,
+  language TEXT
+,
+  pages integer,
+  publisher text,
+  title text,
+  year integer
+);
+
